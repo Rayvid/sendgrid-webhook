b0c889f8b43d29d559a30cf99bcaf50f533ffcc2
Rayvid <gin.kisonas@gmail.com>
2020-05-03 12:35:53 +0300

    Add twillio webhook

---
+++ Twilio/Controllers/SmsController.cs
@@ -0,0 +1,20 @@
+﻿// Code sample for ASP.NET MVC on .NET Framework 4.6.1+
+// In Package Manager, run:
+using System.Web.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace Twilio.Controllers
+{
+    [Route("sms")]
+    public class SmsController : TwilioController
+    {
+        [HttpPost]
+        [Route("sms")]
+        public TwiMLResult Index(SmsRequest incomingMessage)
+        {
+            return new TwiMLResult();
+        }
+    }
+}
